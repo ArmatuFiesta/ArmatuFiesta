e148208e57df66000766d8b25c89df7a9c040236
Jose Luis Moncada <joseluismoncadaostos@gmail.com>
2020-07-22 00:19:00 -0400

    Services pruebas

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+        main: '#f8bbd0',
+      },
+      secondary: {
+        main: '#81d4fa',
+      },
+  },
+});
+
+export default theme;
