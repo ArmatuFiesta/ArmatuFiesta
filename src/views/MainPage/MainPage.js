47519c6b7aeb1269fa2fba0d08fd7898f3867873
Ernesto <48799528+ernestod10@users.noreply.github.com>
2020-07-30 16:13:33 -0400

    cambio nombre

--- src/views/MainPage/MainPage.js
+++ src/views/MainPage/MainPage.js
@@ -19,7 +19,7 @@ const useStyles = makeStyles(styles);
 
 
 export default function MainPage(props) {
-    const serviceCategories=["CATERING","SALONES DE FIESTA","DECORACION",
+    const serviceCategories=["CATERING","NOTARIAS","DECORACION",
     "CORTE Y COSTURA"," OBSEQUIOS Y TARJETERIA","MATRIMONIOS"];
     const contratacionesCategories=["MUSICA", "SHOWS","SEGURIDAD",
 "PROTOCOLO","FOTOGRAFIA","BAILES"];
